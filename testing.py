d7305e1cf00fb979653a7481976398f60e4f0621
unknown <alanlautaro262014@gmail.com>
2020-04-11 17:50:44 -0400

    Version 3

--- testing.py
+++ testing.py
@@ -1,152 +1,183 @@
 '''
-import data, export data, comments
+add breaks, comments, find best time slot for nurses to work
 '''
 '''
-shifts:
+shifts(tests are 1200-2400):
 1. 6am-2pm
 2. 2pm-10pm
 3. 10pm-6am
 4. 6am-6pm
 5. 6pm-6am
-
-tests (12pm-12am):
-1. A (15 minutes)
-2. B (20 minutes)
-3. Both
 '''
-employees_testing = {}
-testing_times_a_1 = {}
-testing_times_a_2 = {}
-testing_times_a = {}
-testing_times_b_unsorted = {}
-testing_times_b = {}
+import pandas as pd
+from collections import OrderedDict
+data = pd.read_excel(r'C:\Users\Alan\Downloads\medical_update_3.12.20_PHS.xls')
+employees = {}
+eid = []
+lname = []
+fname = []
+dep = []
+test_a = []
+test_v = []
+test_rft = []
+times_a = []
+times_v = []
+times_rft = []
+test = [0,0,0]
+test_start_time = [1100,1]
 
-time_a = [1200,1]
-time_a_2 = [1200,1]
-time_b = [1200,1]
-number_employees = int(input("How many employees are you testing: "))
-
-x=1
-while x <= number_employees:
-    name = input("Name: ")
-    shift = int(input("Shift: "))
-    test = int(input("Test: "))
-    info = [shift, test]
-    employees_testing[name] = info
-    x+=1
-def a_time_slot_check(open_time, close_time, test):
-    if test == 'A1':
-        time = time_a
-        testing_times = testing_times_a_1
-    else:
-        time = time_a_2
-        testing_times = testing_times_a_2
-    while time[0]<open_time or time[0]>close_time or str(time[0])+str(time[1]) in testing_times:
-        time[0]+=15
-        if int(str(list(str(time[0]))[2])+str(list(str(time[0]))[3])) == 60:
-            time[0]+=40
-            if time[0] == 2400:
-                time[0] = 1200
-                time[1]+=1
-    return(str(time[1])+str(time[0]))
-
-def b_time_slot_check(open_time, close_time):
-    while time_b[0]<open_time or time_b[0]>close_time or str(time_b[0])+str(time_b[1]) in testing_times_b_unsorted:
-        time_b[0]+=20
-        if int(str(list(str(time_b[0]))[2])+str(list(str(time_b[0]))[3])) == 60:
-            time_b[0]+=40
-            if time_b[0] == 2400:
-                time_b[0] = 1200
-                time_b[1]+=1
-    return(str(time_b[1])+str(time_b[0]))
-
-def c_time_slot_check(open_time,close_time,test):
-    if test == 'A1':
-        temp_time_a = time_a.copy()
-        testing_times = testing_times_a_1.copy()
-    else:
-        temp_time_a = time_a_2.copy()
-        testing_times = testing_times_a_2.copy()
-    temp_time_b = time_b.copy()
-    while temp_time_a[0]<open_time or temp_time_a[0]>close_time or str(temp_time_a[1])+str(temp_time_a[0]) in testing_times:
-        temp_time_a[0]+=15
-        if int(str(list(str(temp_time_a[0]))[2])+str(list(str(temp_time_a[0]))[3])) == 60:
-            temp_time_a[0]+=40
-            if temp_time_a[0] == 2400:
-                temp_time_a[0] = 1200
-                temp_time_a[1]+=1
-    while temp_time_b[0]<open_time or temp_time_b[0]>close_time or str(temp_time_b[1])+str(temp_time_b[0]) in testing_times_b_unsorted or (int(str(temp_time_b[1])+str(temp_time_b[0]))>=int(str(temp_time_a[1])+str(temp_time_a[0])) and int(str(temp_time_b[1])+str(temp_time_b[0]))<=temp_time_a[0]+15) or (int(str(temp_time_b[1])+str(temp_time_b[0]+20))>=int(str(temp_time_a[1])+str(temp_time_a[0])) and int(str(temp_time_b[1])+str(temp_time_b[0]+20))<=int(str(temp_time_a[1])+str(temp_time_a[0]+15))):
-        print(temp_time_b)
-        temp_time_b[0]+=20
-        if int(str(list(str(temp_time_b[0]))[2])+str(list(str(temp_time_b[0]))[3])) == 60:
-            temp_time_b[0]+=40
-            if temp_time_b[0] == 2400:
-                temp_time_b[0] = 1200
-                temp_time_b[1]+=1
-    test_time_a = str(temp_time_a[1])+str(temp_time_a[0])
-    test_time_b = str(temp_time_b[1])+str(temp_time_b[0])
-    return([test_time_a,test_time_b])
-def test_assign(employee, duration, test, shift):
-    global x
-    if shift == 1:
-        open_time = 1200
-        close_time = 1400-duration
-    elif shift == 2:
+def open_close_calculator(shift, duration):
+    if shift == '1':
+        open_time = 1100
+        close_time = 1360-duration
+    elif shift == '2':
         open_time = 1400
-        close_time = 2200-duration
-    elif shift == 3:
+        close_time = 2160-duration
+    elif shift == '3':
         open_time = 2200
-        close_time = 2400-duration
-    elif shift == 1:
-        open_time = 1200
-        close_time = 1800-duration
+        close_time = 2260-duration
+    elif shift == '4':
+        open_time = 1100
+        close_time = 1760-duration
     else:
         open_time = 1800
-        close_time = 2400 - duration
-    if test == 1:
-        if x%2 != 0:
-            test = 'A1'
-            testing_time = a_time_slot_check(open_time, close_time, test)
-            testing_times_a_1[testing_time] = [employee,test]
-            x-=1
-        else:
-            test = 'A2'
-            testing_time = a_time_slot_check(open_time, close_time, test)
-            testing_times_a_2[testing_time] = [employee,test]
-            x-=1
-    elif test == 2:
-        test = 'B'
-        testing_time = b_time_slot_check(open_time, close_time)
-        testing_times_b_unsorted[testing_time] = [employee,test]
-    else:
-        if x%2 != 0:
-            test = 'A1'
-            testing_time = c_time_slot_check(open_time, close_time, test)
-            testing_times_a_1[testing_time[0]] = [employee,test]
-            testing_times_b_unsorted[testing_time[1]] = [employee,test]
-            x-=1
-        else:
-            test = 'A2'
-            testing_time = c_time_slot_check(open_time, close_time, test)
-            testing_times_a_2[testing_time[0]] = [employee,test]
-            testing_times_b_unsorted[testing_time[1]] = [employee,test]
-            x-=1
+        close_time = 2360 - duration
+    return open_time,close_time
+def time_slot_check_a(shift):
+    open_time,close_time = open_close_calculator(shift,7.5)
+    temp_time = test_start_time.copy()
+    while (
+        temp_time[0]<open_time or temp_time[0]>close_time or 
+        (str(temp_time[1])+str(temp_time[0]) in times_a)
+        ):
+        temp_time[0]+=7.5
+        if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) == 60:
+            temp_time[0]+=40
+            if temp_time[0] == 2400:
+                temp_time[0] = 1200
+                temp_time[1]+=1
+    test_time = str(temp_time[1])+str(temp_time[0])
+    times_a.append(test_time)
 
-for employee in employees_testing:
-    test_assign(employee, 15, employees_testing[employee][1], employees_testing[employee][0])
-for x in testing_times_a_1:
-    for y in testing_times_a_2:
-        if x == y:
-            hour = str(list(x)[1])+str(list(x)[2])
-            minute = str(list(x)[3])+str(list(x)[4])
-            day = str(list(x)[0])
-            organised_time = f"{hour}:{minute} Day {day}"
-            testing_times_a[organised_time] = [testing_times_a_1[x][0],testing_times_a_2[x][0]]
-for x in sorted(testing_times_b_unsorted):
-    hour = str(list(x)[1])+str(list(x)[2])
-    minute = str(list(x)[3])+str(list(x)[4])
-    day = str(list(x)[0])
-    organised_time = f"{hour}:{minute} Day {day}"
-    testing_times_b[organised_time] = testing_times_b_unsorted[x][0]
-print(testing_times_a)
-print(testing_times_b)
+    return(date_maker(test_time))
+def time_slot_check_v(test,shift):
+    open_time,close_time = open_close_calculator(shift,7.5)
+    if test[0] == 1:
+        time_a_start = float(str(list(times_a[-1])[0])+str(''.join(list(times_a[-1])[1:])))
+        time_a_end = time_a_start+7.5
+    else:
+        time_a_start=time_a_end=100000
+    temp_time = test_start_time.copy()
+    while (
+        temp_time[0]<open_time or temp_time[0]>close_time or ( 
+        str(temp_time[1])+str(temp_time[0])) in times_v or (
+        float(str(temp_time[1])+str(temp_time[0])) >= time_a_start and (
+        float(str(temp_time[1])+str(temp_time[0])) < time_a_end)
+        )
+        ):
+        temp_time[0]+=7.5
+        if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) == 60:
+            temp_time[0]+=40
+            if temp_time[0] == 2400:
+                temp_time[0] = 1200
+                temp_time[1]+=1
+    test_time = str(temp_time[1])+str(temp_time[0])
+    times_v.append(test_time)
+    return(date_maker(test_time))
+def time_slot_check_rft(test,shift):
+    open_time,close_time = open_close_calculator(shift,20)
+    if test[0] == 1:
+        time_a_start = float(str(list(times_a[-1])[0])+str(''.join(list(times_a[-1])[1:])))
+        time_a_end = time_a_start+7.5
+    else:
+        time_a_start=time_a_end=100000
+    if test[1] == 1:
+        time_b_start = float(str(list(times_v[-1])[0])+str(''.join(list(times_v[-1])[1:])))
+        time_b_end = time_b_start+7.5
+    else:
+        time_b_start=time_b_end=100000
+    temp_time = test_start_time.copy()
+    while (
+        temp_time[0]<open_time or temp_time[0]>close_time or (
+        str(temp_time[1])+str(temp_time[0]) in times_rft) or (
+        (float(str(temp_time[1])+str(temp_time[0])) or float(str(temp_time[1])+str(temp_time[0]+20))) >= (time_a_start or time_b_start)) and (
+        (float(str(temp_time[1])+str(temp_time[0])) or float(str(temp_time[1])+str(temp_time[0]+20))) <= (time_a_end or time_b_end))
+        ):
+        temp_time[0]+=20
+        if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) == 60:
+            temp_time[0]+=40
+            if temp_time[0] == 2400:
+                temp_time[0] = 1200
+                temp_time[1]+=1
+    test_time = str(temp_time[1])+str(temp_time[0])
+    times_rft.append(test_time)
+    return(test_time)
+def test_assign(test, shift):
+    testing_times = []
+    if test[0] == 1:
+        test_a = time_slot_check_a(shift)
+        testing_times.append(test_a)
+    else:
+        test_a = 'N/A'
+        testing_times.append(test_a)
+    if test[1] == 1:
+        test_v = time_slot_check_v(test,shift)
+        testing_times.append(test_v)
+    else:
+        test_v = 'N/A'
+        testing_times.append(test_v)
+    if test[2] == 1:
+        test_rft = time_slot_check_rft(test,shift)
+    else:
+        test_rft = 'N/A'
+    return(test_a,test_v,test_rft)
+def date_maker(time):
+    day = list(time)[0]
+    hour = int(''.join(list(time)[1:3]))
+    minute = ''.join(list(time)[3:5])
+    if hour > 12:
+        test_time = f'{hour-12}:{minute}pm'
+    elif hour == 12:
+        test_time = f'{hour}:{minute}pm'
+    else:
+        test_time = f'{hour}:{minute}am'
+    date = f"Day {day} {test_time}"
+    return date
+for index,column in data.iterrows():
+    if index == 0:
+        continue
+    if type(column[0])!= type(""):
+        continue
+    if column[4] == 'yes':
+        test[0] = 1
+    else:
+        test[0] = 0
+    if column[5] == 'yes':
+        test[1] = 1
+    else:
+        test[1] = 0
+    if column[6] == 'yes':
+        test[2] = 1
+    else:
+        test[2] = 0
+    tests = test_assign(test,column[3])
+    eid.append(column[0])
+    lname.append(column[1])
+    fname.append(column[2])
+    dep.append(column[8])
+    test_a.append(tests[0])
+    test_v.append(tests[1])
+    test_rft.append(tests[2])
+employees = {
+    'EID': eid,
+    'Last Name':lname,
+    'First Name':fname,
+    'Department':dep,
+    'Test A':test_a,
+    'Test V':test_v,
+    'Test RFT':test_rft
+}
+
+employees_df = pd.DataFrame.from_dict(employees)
+employees_df.sort_values(by=['Test RFT'],inplace=True)
+employees_df.to_excel(r"C:\Users\Alan\Downloads\Test_Spreadsheet.xlsx")
