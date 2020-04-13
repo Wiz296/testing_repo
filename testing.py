d0af5590cceef214c7e2f593939fc9c624280f19
unknown <alanlautaro262014@gmail.com>
2020-04-12 20:34:05 -0400

    Version 3 (working)

--- testing.py
+++ testing.py
@@ -24,94 +24,130 @@ times_a = []
 times_v = []
 times_rft = []
 test = [0,0,0]
-test_start_time = [1100,1]
+test_start_time = [340.0,1]
 
 def open_close_calculator(shift, duration):
     if shift == '1':
-        open_time = 1100
+        open_time = 600
         close_time = 1360-duration
     elif shift == '2':
         open_time = 1400
-        close_time = 2160-duration
+        close_time = 1540-duration
     elif shift == '3':
-        open_time = 2200
-        close_time = 2260-duration
+        open_time = 340
+        close_time = 560-duration
     elif shift == '4':
-        open_time = 1100
-        close_time = 1760-duration
+        open_time = 600
+        close_time = 1540-duration
     else:
-        open_time = 1800
-        close_time = 2360 - duration
+        open_time = 340
+        close_time = 560 - duration
     return open_time,close_time
 def time_slot_check_a(shift):
     open_time,close_time = open_close_calculator(shift,7.5)
     temp_time = test_start_time.copy()
     while (
         temp_time[0]<open_time or temp_time[0]>close_time or 
-        (str(temp_time[1])+str(temp_time[0]) in times_a)
+        (str(temp_time[1])+str(temp_time[0]) in times_a) or
+        (str(temp_time[1])+'0'+str(temp_time[0]) in times_a)
         ):
         temp_time[0]+=7.5
-        if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) == 60:
-            temp_time[0]+=40
-            if temp_time[0] == 2400:
-                temp_time[0] = 1200
-                temp_time[1]+=1
-    test_time = str(temp_time[1])+str(temp_time[0])
+        if temp_time[0]<1000:
+            if int(str(list(str(temp_time[0]))[1])+str(list(str(temp_time[0]))[2])) >= 60:
+                temp_time[0]+=40
+                if temp_time[0] >= 2400:
+                    temp_time[0] = test_start_time[0]
+                    temp_time[1]+=1
+        else:
+            if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) >= 60:
+                temp_time[0]+=40
+                if temp_time[0] >= 2400:
+                    temp_time[0] = test_start_time[0]
+                    temp_time[1]+=1
+    if temp_time[0]<1000:
+        test_time = str(temp_time[1])+'0'+str(temp_time[0])
+    else:
+        test_time = str(temp_time[1])+str(temp_time[0])
     times_a.append(test_time)
-
     return(date_maker(test_time))
 def time_slot_check_v(test,shift):
     open_time,close_time = open_close_calculator(shift,7.5)
     if test[0] == 1:
-        time_a_start = float(str(list(times_a[-1])[0])+str(''.join(list(times_a[-1])[1:])))
+        if int(list(times_a[-1])[1])!=1:
+            time_a_start = float(str(list(times_a[-1])[0])+str(float(''.join(list(times_a[-1])[1:]))))
+        else:
+            time_a_start = float(str(list(times_a[-1])[0])+str(''.join(list(times_a[-1])[1:])))
         time_a_end = time_a_start+7.5
     else:
         time_a_start=time_a_end=100000
     temp_time = test_start_time.copy()
     while (
-        temp_time[0]<open_time or temp_time[0]>close_time or ( 
-        str(temp_time[1])+str(temp_time[0])) in times_v or (
+        temp_time[0]<open_time or temp_time[0]>close_time or (
+        str(temp_time[1])+str(temp_time[0]) in times_v) or (
+        str(temp_time[1])+'0'+str(temp_time[0]) in times_v) or (
         float(str(temp_time[1])+str(temp_time[0])) >= time_a_start and (
-        float(str(temp_time[1])+str(temp_time[0])) < time_a_end)
-        )
+        float(str(temp_time[1])+str(temp_time[0])) < time_a_end))
         ):
         temp_time[0]+=7.5
-        if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) == 60:
-            temp_time[0]+=40
-            if temp_time[0] == 2400:
-                temp_time[0] = 1200
-                temp_time[1]+=1
-    test_time = str(temp_time[1])+str(temp_time[0])
+        if temp_time[0]<1000:
+            if int(str(list(str(temp_time[0]))[1])+str(list(str(temp_time[0]))[2])) >= 60:
+                temp_time[0]+=40
+                if temp_time[0] >= 2400:
+                    temp_time[0] = test_start_time[0]
+                    temp_time[1]+=1
+        else:
+            if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) >= 60:
+                temp_time[0]+=40
+                if temp_time[0] >= 2400:
+                    temp_time[0] = test_start_time[0]
+                    temp_time[1]+=1
+    if temp_time[0]<1000:
+        test_time = str(temp_time[1])+'0'+str(temp_time[0])
+    else:
+        test_time = str(temp_time[1])+str(temp_time[0])
     times_v.append(test_time)
     return(date_maker(test_time))
 def time_slot_check_rft(test,shift):
     open_time,close_time = open_close_calculator(shift,20)
     if test[0] == 1:
-        time_a_start = float(str(list(times_a[-1])[0])+str(''.join(list(times_a[-1])[1:])))
+        if int(list(times_a[-1])[1])!=1:
+            time_a_start = float(str(list(times_a[-1])[0])+str(float(''.join(list(times_a[-1])[1:]))))
+        else:
+            time_a_start = float(str(list(times_a[-1])[0])+str(''.join(list(times_a[-1])[1:])))
         time_a_end = time_a_start+7.5
     else:
         time_a_start=time_a_end=100000
     if test[1] == 1:
-        time_b_start = float(str(list(times_v[-1])[0])+str(''.join(list(times_v[-1])[1:])))
-        time_b_end = time_b_start+7.5
+        if int(list(times_v[-1])[1])!=1:
+            time_v_start = float(str(list(times_v[-1])[0])+str(float(''.join(list(times_v[-1])[1:]))))
+        else:
+            time_v_start = float(str(list(times_v[-1])[0])+str(''.join(list(times_v[-1])[1:])))
+        time_v_end = time_v_start+7.5
     else:
-        time_b_start=time_b_end=100000
+        time_v_start=time_v_end=100000
     temp_time = test_start_time.copy()
     while (
         temp_time[0]<open_time or temp_time[0]>close_time or (
         str(temp_time[1])+str(temp_time[0]) in times_rft) or (
-        (float(str(temp_time[1])+str(temp_time[0])) or float(str(temp_time[1])+str(temp_time[0]+20))) >= (time_a_start or time_b_start)) and (
-        (float(str(temp_time[1])+str(temp_time[0])) or float(str(temp_time[1])+str(temp_time[0]+20))) <= (time_a_end or time_b_end))
+        (float(str(temp_time[1])+str(temp_time[0])) or float(str(temp_time[1])+str(temp_time[0]+20))) >= (time_a_start or time_v_start) and (
+        (float(str(temp_time[1])+str(temp_time[0])) or float(str(temp_time[1])+str(temp_time[0]+20))) <= (time_a_end or time_v_end)))
         ):
         temp_time[0]+=20
-        if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) == 60:
-            temp_time[0]+=40
-            if temp_time[0] == 2400:
-                temp_time[0] = 1200
-                temp_time[1]+=1
+        if temp_time[0]<1000:
+            if int(str(list(str(temp_time[0]))[1])+str(list(str(temp_time[0]))[2])) >= 60:
+                temp_time[0]+=40
+                if temp_time[0] >= 2400:
+                    temp_time[0] = test_start_time[0]
+                    temp_time[1]+=1
+        else:
+            if int(str(list(str(temp_time[0]))[2])+str(list(str(temp_time[0]))[3])) >= 60:
+                temp_time[0]+=40
+                if temp_time[0] >= 2400:
+                    temp_time[0] = test_start_time[0]
+                    temp_time[1]+=1
     test_time = str(temp_time[1])+str(temp_time[0])
     times_rft.append(test_time)
-    return(test_time)
+    return(date_maker(test_time))
 def test_assign(test, shift):
     testing_times = []
     if test[0] == 1:
@@ -133,15 +169,11 @@ def test_assign(test, shift):
     return(test_a,test_v,test_rft)
 def date_maker(time):
     day = list(time)[0]
-    hour = int(''.join(list(time)[1:3]))
-    minute = ''.join(list(time)[3:5])
-    if hour > 12:
-        test_time = f'{hour-12}:{minute}pm'
-    elif hour == 12:
-        test_time = f'{hour}:{minute}pm'
+    if int(list(time)[1])!=1:
+        hour = '0'+str(int(float(''.join(list(time)[1:]))))
     else:
-        test_time = f'{hour}:{minute}am'
-    date = f"Day {day} {test_time}"
+        hour = int(float(''.join(list(time)[1:])))
+    date = f"Day {day} {hour}"
     return date
 for index,column in data.iterrows():
     if index == 0:
@@ -179,5 +211,5 @@ employees = {
 }
 
 employees_df = pd.DataFrame.from_dict(employees)
-employees_df.sort_values(by=['Test RFT'],inplace=True)
+employees_df.sort_values(by=['Test A'],inplace=True)
 employees_df.to_excel(r"C:\Users\Alan\Downloads\Test_Spreadsheet.xlsx")
